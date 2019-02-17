04df152f6c7bd247d05e19e5779d79bbc3f8b851
mscandlen3 <mscandlen3@gatech.edu>
2019-02-17 16:55:02 -0500

    Add Save Method

--- app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
@@ -1,12 +1,17 @@
 package macbookpro.cs2340.spacetrader.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import macbookpro.cs2340.spacetrader.R;
+import macbookpro.cs2340.spacetrader.model.GameDifficulty;
 import macbookpro.cs2340.spacetrader.viewmodels.ConfigurationViewModel;
 
 public class ConfigurationActivity extends AppCompatActivity {
@@ -25,12 +30,30 @@ public class ConfigurationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.config.xml);
+        setContentView(R.layout.config);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
+    }
 
+    private void saveConfiguration() {
+        String name = inputName.getText().toString();
+        GameDifficulty level = (GameDifficulty) difficultySpinner.getSelectedItem();
 
+        if (name.trim().isEmpty()) {
+            Toast.makeText(this, "Fields cannot be blank", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Log.d("APP", "Making intent");
+        Log.d("APP", "Data - " + name + " " + " " + level.toString());
+        Intent intent = new Intent();
+        intent.putExtra(name);
+        intent.putExtra(level);
+
+        setResult(RESULT_OK, intent);
+
+        finish();
 
     }
 
