560299770a1266ea0fc167e3b15e031a944934f8
Hamna <aichkay01@gmail.com>
2019-02-17 16:51:32 -0500

    added FindViewByIDs

--- app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
@@ -3,6 +3,7 @@ package macbookpro.cs2340.spacetrader.views;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -28,6 +29,22 @@ public class ConfigurationActivity extends AppCompatActivity {
         setContentView(R.layout.config.xml);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+
+        /*
+         * Grab the dialog widgets so we can get info for later
+         */
+        playerNameLabel = findViewById(R.id.player_name_label);
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
+        inputName = findViewById(R.id.input_name);
+
+        Button addPilot = findViewById(R.id.add_pilot_skill_button);
+        Button subtractPilot = findViewById(R.id.subtract_pilot_skill_button);
+        Button addFighter = findViewById(R.id.add_fighter_skill_button);
+        Button subtractFighter = findViewById(R.id.subtract_fighter_skill_button);
+        Button addTrader = findViewById(R.id.add_Trader_skill_button);
+        Button subtractTrader = findViewById(R.id.subtract_trader_skill_button);
+        Button addEngineer = findViewById(R.id.add_engineer_skill_button);
+        Button subtractEngineer = findViewById(R.id.subtract_engineer_skill_button);
 
 
 
