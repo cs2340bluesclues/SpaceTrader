5dac827e86f46de7f4ad60ec336b5931552a468c
msandry3 <msandry3@gatech.edu>
2019-02-18 16:02:41 -0500

    Getters and setters

--- app/src/main/java/macbookpro/cs2340/spacetrader/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/viewmodels/ConfigurationViewModel.java
@@ -8,22 +8,72 @@ import macbookpro.cs2340.spacetrader.model.Player;
 
 public class ConfigurationViewModel {
 
-    private int count;
+    private int totalCount;
+
+    private int pilotCount;
+
+    private int fighterCount;
+
+    private int traderCount;
+
+    private int engineerCount;
 
     public ConfigurationViewModel() {
-        count = 0;
+        totalCount = 0;
+        pilotCount = 0;
+        fighterCount = 0;
+        traderCount = 0;
+        engineerCount = 0;
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public void setPilotCount(int count) {
+        this.pilotCount = count;
+        updateTotalCount();
     }
 
-    public int getCount() {
-        return count;
+    public void setFighterCount(int count) {
+        this.fighterCount = count;
+        updateTotalCount();
     }
 
-    public boolean checkCount() {
-        return count < 16;
+    public void setTraderCount(int count) {
+        this.traderCount = count;
+        updateTotalCount();
     }
+
+    public void setEngineerCount(int count) {
+        this.engineerCount = count;
+        updateTotalCount();
+    }
+
+    private void updateTotalCount() {
+        totalCount = pilotCount + fighterCount + traderCount + engineerCount;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public int getPilotCount() {
+        return pilotCount;
+    }
+
+    public int getFighterCount() {
+        return fighterCount;
+    }
+
+    public int getTraderCount() {
+        return traderCount;
+    }
+
+    public int getEngineerCount() {
+        return engineerCount;
+    }
+
+    public boolean checkCountGreater16() {
+        return totalCount <= 16;
+    }
+
+    public boolean checkCountLess0() { return totalCount >= 0; }
 
 }
