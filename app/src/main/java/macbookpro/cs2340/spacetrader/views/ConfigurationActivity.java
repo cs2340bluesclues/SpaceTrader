b1f07138be92bf19e04111f91dae81c5cd79fcd0
Hannah Kim <hannahkim0114@gmail.com>
2019-02-18 11:21:40 -0500

    clicking buttons updates skillpoints

--- app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
@@ -29,7 +29,10 @@ public class ConfigurationActivity extends AppCompatActivity {
     private TextView playerNameLabel;
     private EditText inputName;
     private Spinner difficultySpinner;
-
+    //some experimental shit
+    Button addPilot, subtractPilot, addFighter, subtractFighter, addTrader, subtractTrader, addEngineer, subtractEngineer;
+    TextView pilotSkill, fighterSkill, traderSkill, engineerSkill;
+    int pilotCounter, fighterCounter, traderCounter, engineerCounter, totalCounter;
     //new player??
     private Player player;
     //private ;
@@ -48,21 +51,108 @@ public class ConfigurationActivity extends AppCompatActivity {
         difficultySpinner = findViewById(R.id.difficulty_spinner);
         inputName = findViewById(R.id.input_name);
 
-        Button addPilot = findViewById(R.id.add_pilot_skill_button);
-        Button subtractPilot = findViewById(R.id.subtract_pilot_skill_button);
-        Button addFighter = findViewById(R.id.add_fighter_skill_button);
-        Button subtractFighter = findViewById(R.id.subtract_fighter_skill_button);
-        Button addTrader = findViewById(R.id.add_trader_skill_button);
-        Button subtractTrader = findViewById(R.id.subtract_trader_skill_button);
-        Button addEngineer = findViewById(R.id.add_engineer_skill_button);
-        Button subtractEngineer = findViewById(R.id.subtract_engineer_skill_button);
+        addPilot = (Button) findViewById(R.id.add_pilot_skill_button);
+        subtractPilot = (Button) findViewById(R.id.subtract_pilot_skill_button);
+        pilotSkill = (TextView) findViewById(R.id.pilot_skill_tracker);
+        pilotCounter = 0;
+
+        addFighter = findViewById(R.id.add_fighter_skill_button);
+        subtractFighter = findViewById(R.id.subtract_fighter_skill_button);
+        fighterSkill = findViewById(R.id.fighter_skill_tracker);
+        fighterCounter = 0;
+
+        addTrader = findViewById(R.id.add_trader_skill_button);
+        subtractTrader = findViewById(R.id.subtract_trader_skill_button);
+        traderSkill = findViewById(R.id.trader_skill_tracker);
+        traderCounter = 0;
+
+        addEngineer = findViewById(R.id.add_engineer_skill_button);
+        subtractEngineer = findViewById(R.id.subtract_engineer_skill_button);
+        engineerSkill = findViewById(R.id.engineer_skill_tracker);
+        engineerCounter = 0;
+
+
         Button submit = findViewById(R.id.submit_player_info_button);
 
         //makes difficulties visible in the spinner
         ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, GameDifficulty.values());
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         difficultySpinner.setAdapter(adapter);
+
+        //updates the textview of each skill, still need to figure out how to cap at 16 total
+        addPilot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pilotCounter = pilotCounter + 1;
+                pilotSkill.setText("" + pilotCounter);
+            }
+        });
+
+        subtractPilot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pilotCounter > 0) {
+                    pilotCounter = pilotCounter - 1;
+                    pilotSkill.setText("" + pilotCounter);
+                }
+            }
+        });
+
+        addFighter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fighterCounter = fighterCounter + 1;
+                fighterSkill.setText("" + fighterCounter);
+            }
+        });
+
+        subtractFighter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (fighterCounter > 0) {
+                    fighterCounter = fighterCounter - 1;
+                    fighterSkill.setText("" + fighterCounter);
+                }
+            }
+        });
+
+        addTrader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                traderCounter = traderCounter + 1;
+                traderSkill.setText("" + traderCounter);
+            }
+        });
+
+        subtractTrader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (traderCounter > 0) {
+                    traderCounter = traderCounter - 1;
+                    traderSkill.setText("" + traderCounter);
+                }
+            }
+        });
+
+        addEngineer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                engineerCounter = engineerCounter + 1;
+                engineerSkill.setText("" + engineerCounter);
+            }
+        });
+
+        subtractEngineer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (engineerCounter > 0) {
+                    engineerCounter = engineerCounter - 1;
+                    engineerSkill.setText("" + engineerCounter);
+                }
+            }
+        });
     }
+
 
         //ok doesn't work yet but think this is next step to store the data??
 //    public void onSubmitPressed(View view) {
