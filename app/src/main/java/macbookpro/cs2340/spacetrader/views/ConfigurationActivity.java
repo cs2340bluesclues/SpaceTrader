2952846a38b328d521e571440b60c2a969d29a95
Hannah Kim <hannahkim0114@gmail.com>
2019-02-17 21:47:41 -0500

    made submit button and difficulty spinner values show up

--- app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/views/ConfigurationActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.util.Log;
 import android.widget.EditText;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 
 import macbookpro.cs2340.spacetrader.R;
 import macbookpro.cs2340.spacetrader.model.GameDifficulty;
+import macbookpro.cs2340.spacetrader.model.Player;
 import macbookpro.cs2340.spacetrader.viewmodels.ConfigurationViewModel;
 
 public class ConfigurationActivity extends AppCompatActivity {
@@ -26,6 +29,9 @@ public class ConfigurationActivity extends AppCompatActivity {
     private TextView playerNameLabel;
     private EditText inputName;
     private Spinner difficultySpinner;
+
+    //new player??
+    private Player player;
     //private ;
 
     @Override
@@ -50,9 +56,21 @@ public class ConfigurationActivity extends AppCompatActivity {
         Button subtractTrader = findViewById(R.id.subtract_trader_skill_button);
         Button addEngineer = findViewById(R.id.add_engineer_skill_button);
         Button subtractEngineer = findViewById(R.id.subtract_engineer_skill_button);
+        Button submit = findViewById(R.id.submit_player_info_button);
 
+        //makes difficulties visible in the spinner
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, GameDifficulty.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(adapter);
     }
 
+        //ok doesn't work yet but think this is next step to store the data??
+//    public void onSubmitPressed(View view) {
+//        Log.d("submit", "info submitted");
+//        player.setName(playerNameLabel.getText().toString());
+//        player.setDifficulty((String) difficultySpinner.getSelectedItem()); // not sure if this should be a string
+//        Log.d("submit", "new player data: " + player);
+//    }
     private void saveConfiguration() {
         String name = inputName.getText().toString();
         GameDifficulty level = (GameDifficulty) difficultySpinner.getSelectedItem();
