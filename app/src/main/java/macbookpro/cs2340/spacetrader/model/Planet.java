dcf6099c1ad6ed777ea9bc593cd631bd55a87ed0
mscandlen3 <mscandlen3@gatech.edu>
2019-02-20 20:33:00 -0500

    Create Planet class

---
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Planet.java
@@ -0,0 +1,11 @@
+package macbookpro.cs2340.spacetrader.model;
+
+public class Planet {
+
+    private String name;
+
+    public Planet(String name) {
+        this.name = name;
+    }
+
+}
--- app/src/main/java/macbookpro/cs2340/spacetrader/model/SolarSystem.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/SolarSystem.java
@@ -1,5 +1,13 @@
 package macbookpro.cs2340.spacetrader.model;
 
+import java.util.HashSet;
+
 public class SolarSystem {
+
+    private HashSet<Planet> planets;
+
+    public SolarSystem() {
+        planets = new HashSet<>();
+    }
 
 }
