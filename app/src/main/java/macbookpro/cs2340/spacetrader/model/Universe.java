97c955b53ec326edf24ff11d6e86016aa17df938
mscandlen3 <mscandlen3@gatech.edu>
2019-02-20 20:22:48 -0500

    Create SolarSystem and Univese class

---
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
@@ -0,0 +1,12 @@
+package macbookpro.cs2340.spacetrader.model;
+
+import java.util.HashSet;
+
+public class Universe {
+    private HashSet<SolarSystem> system;
+
+    public Universe() {
+        system = new HashSet<>();
+    }
+
+}
