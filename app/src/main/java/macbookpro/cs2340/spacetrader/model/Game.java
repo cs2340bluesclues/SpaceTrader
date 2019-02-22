0192a0ae22b18ea847cde557353ecee7615fbcb2
mscandlen3 <mscandlen3@gatech.edu>
2019-02-22 11:42:21 -0500

    Add list of solarsystem names

--- app/src/main/java/macbookpro/cs2340/spacetrader/model/Game.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Game.java
@@ -1,6 +1,9 @@
 package macbookpro.cs2340.spacetrader.model;
 
+import java.util.Random;
+
 public class Game {
+
     private GameDifficulty diff;
 
     public Game(GameDifficulty diff) {
@@ -14,5 +17,11 @@ public class Game {
 
     public GameDifficulty getDiff() {
         return diff;
+    }
+
+    public Universe generateUniverse(int seed) {
+        Random random = new Random(seed);
+
+        return null;
     }
 }
--- app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
@@ -1,16 +1,44 @@
 package macbookpro.cs2340.spacetrader.model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Universe {
+
     private HashSet<SolarSystem> system;
+    private List<String> names;
 
     public Universe() {
         system = new HashSet<>();
+        readList();
     }
 
-    public void addSolarSystem(SolarSystem s) {
-        system.add(s);
+    private void readList() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("./Names"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line;
+        while ((line = reader.readLine()) != null) {
+            names.add(line);
+        }
+        reader.close();
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+    }
+
+    public void addSolarSystem() {
+        system.add(new SolarSystem("n", new Coord(1, 0)));
     }
 
     public void travel() {
