7e8c596d6c4c98582a878026d30347650dd63749
mscandlen3 <mscandlen3@gatech.edu>
2019-02-22 13:32:25 -0500

    Add array with all names

--- app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
@@ -1,12 +1,6 @@
 package macbookpro.cs2340.spacetrader.model;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,17 +15,24 @@ public class Universe {
     }
 
     private void readList() {
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader("./Names"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String line;
-        while ((line = reader.readLine()) != null) {
-            names.add(line);
-        }
-        reader.close();
+
+        String arr[] = new String[]{"Aldea", "Andevian", "Baratas", "Brax", "Calondia", "Capelle",
+                "Carzon", "Cestus", "Cheron", "Damast", "Draylon", "Drema", "Endor", "Esmee",
+                "Exo", "Ferris", "Fourmi", "Frolix", "Gemulon", "Helena", "Iralius", "Janus",
+                "Japori", "Jarada", "Jason", "Kaylon", "Keanu", "Khefka", "Kira", "Klaatu",
+                "Korma", "Kravat", "Krios", "Malcoria", "Melina", "Merik", "Mintaka", "Montor",
+                "Mordan", "Myrthe", "Nelvana", "Nix", "Nile", "Parade", "Picard", "Quator",
+                "Rakhar", "Rhymus", "Rochani", "Rubicum", "Sarpeidon", "Sefalla", "Sol", "Stakoron",
+                "Styris", "Tantalos", "Tarchannen", "Thera", "Titan", "Triacus", "Tyrus", "Vandor",
+                "Ventax", "Xenon", "Xerxes", "Yojimbo", "Zalkon", "Zuul"};
+        names = Arrays.asList();
+//        BufferedReader reader;
+//        reader = new BufferedReader(new FileReader("./Names"));
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            names.add(line);
+//        }
+//        reader.close();
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
         }
