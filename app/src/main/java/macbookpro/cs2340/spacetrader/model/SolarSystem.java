8f0bd13144a611bbc203db9750fd3121fd7177bd
mscandlen3 <mscandlen3@gatech.edu>
2019-02-23 13:51:40 -0500

    Adds solarsystems to Universe

--- app/src/main/java/macbookpro/cs2340/spacetrader/model/SolarSystem.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/SolarSystem.java
@@ -23,14 +23,29 @@ public class SolarSystem {
     private HashSet<Planet> planets;
     private String name;
     private Coord coords;
+    private int numPlanets;
+    private Random r;
 
-    public SolarSystem(int seed) {
-        Random r = new Random(seed);
+    public SolarSystem(Random random) {
+        r = random;
         this.name = nameList.remove(r.nextInt(nameList.size()));
-        //I'm not sure if this works correctly
+
+        // Not sure if this works correctly
         Coord temp = new Coord(r.nextInt(16), r.nextInt(16));
         coordsMap.put(temp, temp.hashCode());
+        coords = temp;
+
+        // populates solar system with planets
         planets.add(new Planet(name));
+        numPlanets = r.nextInt(5);
+        for (int i = 0; i < numPlanets; i++) {
+            addPlanet();
+        }
+    }
+
+    private void addPlanet() {
+        String planetName = nameList.remove(r.nextInt(nameList.size()));
+        planets.add(new Planet(planetName));
     }
 
 }
--- app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Universe.java
@@ -1,17 +1,23 @@
 package macbookpro.cs2340.spacetrader.model;
 
 import java.util.HashSet;
+import java.util.Random;
 
 public class Universe {
 
     private HashSet<SolarSystem> system;
+    private Random r;
+    private int numSolarSystems;
 
-    public Universe() {
+    public Universe(int n, int seed) {
         system = new HashSet<>();
-    }
-
-    public void addSolarSystem(int seed) {
-        SolarSystem s1 = new SolarSystem(seed);
+        r = new Random(seed);
+        numSolarSystems = n;
+        for (int i = 0; i < numSolarSystems; i++) {
+            system.add(new SolarSystem(r));
+            seed++;
+            r.setSeed(seed);
+        }
     }
 
     public void travel() {
