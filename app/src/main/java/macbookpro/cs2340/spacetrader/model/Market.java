cedc4cec96fe0096636246e4ae76f5567bedfc22
Hamna <aichkay01@gmail.com>
2019-02-27 17:32:08 -0500

    Added market, market item, police, pirate classes

---
+++ app/src/main/java/macbookpro/cs2340/spacetrader/model/Market.java
@@ -0,0 +1,12 @@
+package macbookpro.cs2340.spacetrader.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Market {
+    List<MarketItem> marketItems = new ArrayList<>();
+
+    public Market() {
+        //initialize marketItems
+    }
+}
