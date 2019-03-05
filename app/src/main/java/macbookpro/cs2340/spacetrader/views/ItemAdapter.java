5a36e6cca8efd671db49617e98e28ae51274aef1
mscandlen3 <mscandlen3@gatech.edu>
2019-03-04 21:45:45 -0500

    Add methods to ItemAdapter

--- app/src/main/java/macbookpro/cs2340/spacetrader/views/ItemAdapter.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/views/ItemAdapter.java
@@ -11,23 +11,35 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import macbookpro.cs2340.spacetrader.R;
 import macbookpro.cs2340.spacetrader.model.MarketInfo;
+import macbookpro.cs2340.spacetrader.model.MarketItem;
 
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder>{
 
     public List<MarketInfo> marketInfoList = new ArrayList<>();
 
+    private OnMarketInfoClickListener listener;
+
     @NonNull
     @Override
-    public ItemViewHolder onItemViewHolder(@NonNull ViewGroup parent, int i) {
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
 
         //Tell the adapter what layout to use for each course in the list
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.market_item, parent, false);
 
         return new ItemViewHolder(itemView);
-    })
+    }
+
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+
+        MarketInfo item = marketInfoList.get(position);
+
+        holder.price.setText(item.getPrice());
+        holder.quantity.setText(item.getQuantity());
+    }
 
     @Override
     public int getItemCount() {
@@ -37,19 +49,45 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         return marketInfoList.size();
     }
 
+    public void setMarketInfoList(List<MarketInfo> items) {
+        marketInfoList = items;
+        notifyDataSetChanged();
+    }
+
+    public MarketInfo getMarketInfo(int position) { return marketInfoList.get(position); }
+
 
     class ItemViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView resourceName;
+        private TextView name;
         private TextView quantity;
         private TextView price;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
-            resourceName = itemView.findViewById(R.id.text_resource_name);
+            name = itemView.findViewById(R.id.text_resource_name);
             quantity = itemView.findViewById(R.id.text_quantity);
             price = itemView.findViewById(R.id.text_price);
-        }
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        listener.onMarketInfoClicked(marketInfoList.get(position));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnMarketInfoClickListener {
+        void onMarketInfoClicked(MarketInfo marketInfo);
+    }
+
+    public void setOnMarketInfoClickListener(OnMarketInfoClickListener listener) {
+        this.listener = listener;
     }
 }
