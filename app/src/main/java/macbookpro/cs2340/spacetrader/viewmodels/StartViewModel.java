a99057c3b367c0bc4d36c1ce09c1daa5a8a49bf8
Saachi Wadhwani <30535956+saachiw@users.noreply.github.com>
2019-03-08 16:52:21 -0500

    model facade accesses market info list now

---
+++ app/src/main/java/macbookpro/cs2340/spacetrader/viewmodels/StartViewModel.java
@@ -0,0 +1,17 @@
+package macbookpro.cs2340.spacetrader.viewmodels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+import java.util.List;
+
+import macbookpro.cs2340.spacetrader.model.MarketInfo;
+import macbookpro.cs2340.spacetrader.model.MarketItem;
+
+public class StartViewModel extends AndroidViewModel {
+
+
+
+
+
+}
