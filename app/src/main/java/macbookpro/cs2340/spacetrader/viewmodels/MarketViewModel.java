400cca4e27c416ff8b063d7587432511e66bc0bf
mscandlen3 <mscandlen3@gatech.edu>
2019-03-28 18:43:06 -0400

    Removed a few lines

--- app/src/main/java/macbookpro/cs2340/spacetrader/viewmodels/MarketViewModel.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/viewmodels/MarketViewModel.java
@@ -4,15 +4,11 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 import android.util.Log;
-import android.util.Pair;
 
-import java.util.List;
 import java.util.Map;
-
 
 import macbookpro.cs2340.spacetrader.model.MarketInfo;
 import macbookpro.cs2340.spacetrader.model.Market;
-import macbookpro.cs2340.spacetrader.model.ModelFacade;
 import macbookpro.cs2340.spacetrader.model.Player;
 import macbookpro.cs2340.spacetrader.model.Universe.Planet;
 
