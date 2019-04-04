cfc0822e8944e6201578c7cc2120c0fbc6c94db3
msandry3 <msandry3@gatech.edu>
2019-04-03 21:15:55 -0400

    xml files and manifest

--- app/src/main/java/macbookpro/cs2340/spacetrader/views/PirateActivity.java
+++ app/src/main/java/macbookpro/cs2340/spacetrader/views/PirateActivity.java
@@ -3,9 +3,12 @@ package macbookpro.cs2340.spacetrader.views;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import macbookpro.cs2340.spacetrader.R;
+
 public class PirateActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.pirate_activity);
     }
 }
